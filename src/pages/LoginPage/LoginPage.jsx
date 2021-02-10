0c93d58e2c2aa653127d5e79847169c15a01787b
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-09 22:41:22 -0500

    add basic login link

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+const LoginPage = (props) => {
+  return (
+    <div>
+      <Link to='/login'>Login</Link>
+    </div>
+  )
+}
+
+export default LoginPage;

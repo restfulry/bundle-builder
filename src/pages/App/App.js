7a8af5557a70c5be74908bb3bde19cad0bbe136a
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-09 22:42:56 -0500

    add basic link to signup page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 import "./App.css";
 
 import LoginPage from "../LoginPage/LoginPage";
+import SignupPage from "../SignupPage/SignupPage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <LoginPage />
+        <SignupPage />
       </div>
     );
   }
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+const SignupPage = (props) => {
+  return (
+    <div>
+      <Link to='/signup'>Signup</Link>
+    </div>
+  )
+}
+
+export default SignupPage;

d08b13063fdf00ebb8e80551feb743948d03cec5
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-10 19:35:06 -0500

    connect mongo db

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 
+require("dotenv").config();
+require("./config/database");
+
 const app = express();
 
 app.use(logger("dev"));
@@ -21,7 +24,7 @@ app.get("/*", function (req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);

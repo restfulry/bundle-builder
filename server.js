8c0e02b5b3fa0c097b9897e7ef90f71f5afe0d52
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-10 19:51:02 -0500

    remove favicon from default

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const favicon = require("serve-favicon");
 const logger = require("morgan");
 
 require("dotenv").config();
@@ -11,19 +10,14 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 
-// Put API routes here, before the "catch all" route
+// API Routes
 
-// The following "catch all" route (note the *)is necessary
-// for a SPA's client-side routing to properly work
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
 const port = process.env.PORT || 5000;
 
 app.listen(port, function () {

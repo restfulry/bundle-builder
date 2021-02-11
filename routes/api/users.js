113b04c4590f91a3e9b928c761f9d44687a5fa6c
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-11 11:20:47 -0500

    add checkAuth helper fn in routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,4 +9,10 @@ router.post("/login", usersCtrl.login);
 
 /*---------- Protected Routes ----------*/
 
+/*----- Helper Functions -----*/
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({ msg: "Not Authorized" });
+}
+
 module.exports = router;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 
 import LoginPage from "../LoginPage/LoginPage";

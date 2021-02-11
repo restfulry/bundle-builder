b7d1c824a7fb030e838269581ca0d62c3896a274
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-10 22:11:52 -0500

    persist jwt

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -2,13 +2,15 @@ const BASE_URL = "/api/users/";
 
 function signup(user) {
   return fetch(BASE_URL + "signup", {
-    method: postMessage,
+    method: "POST",
     headers: new Headers({ "Content-Type": "application/json" }),
     body: JSON.stringify(user),
-  }).then((res) => {
-    if (res.ok) return res.json();
-    throw new Error("Email already taken");
-  });
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      throw new Error("Email already taken");
+    })
+    .then((token) => token.token);
   // .then(({ token }) => tokenService.setToken(token));
 }
 

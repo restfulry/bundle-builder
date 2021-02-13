3ba2deee008b7c3324b548a47cf6ccbfc52f823e
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-13 14:19:55 -0500

    add product model

---
+++ models/product.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    productName: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, default: 0, required: true },
+    tags: { type: String, required: true },
+    storeOwner: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -35,29 +35,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar user={this.state.user} handleLogout={this.handleLogout} />
-
-        <Route
-          exact
-          path="/signup"
-          render={({ history }) => (
-            <SignupPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/login"
-          render={({ history }) => (
-            <LoginPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />
-          )}
-        />
-        <Route exact path="/logout" render={({ history }) => <LogoutPage />} />
+        <Switch>
+          <NavBar user={this.state.user} handleLogout={this.handleLogout} />
+          <Route
+            exact
+            path="/signup"
+            render={({ history }) => (
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={({ history }) => (
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/logout"
+            render={({ history }) => <LogoutPage />}
+          />
+        </Switch>
       </div>
     );
   }

46f519c76de9dad1abbdaa2ebde1863a85d851d0
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-13 16:40:35 -0500

    add product form complete

--- src/pages/AddProductPage/AddProductPage.jsx
+++ src/pages/AddProductPage/AddProductPage.jsx
@@ -31,6 +31,30 @@ class AddProductPage extends Component {
     return (
       <div>
         <h1>Add Product</h1>
+        <form ref={this.formRef} autoComplete="off" onSubmit={this.handlSubmit}>
+          
+          <div className="form-group">
+            <label>Name</label>
+            <input className="form-control" name="productName" value={this.state.formData.productName} onChange={this.handleChange} required />
+          </div>
+
+          <div className="form-group">
+            <label>Description</label>
+            <input className="form-control" name="description" value={this.state.formData.description} onChange={this.handleChange} required />
+          </div>
+
+          <div className="form-group">
+            <label>Price</label>
+            <input className="form-control" name="price" value={this.state.formData.price} onChange={this.handleChange} required />
+          </div>
+
+          <div className="form-group">
+            <label>Tags</label>
+            <input className="form-control" name="tags" value={this.state.formData.tags} onChange={this.handleChange} required />
+          </div>
+
+          <button type="submit" className="btn" disabled={this.state.invalidForm}>Add Product</button>
+        </form>
       </div>
     )
   }

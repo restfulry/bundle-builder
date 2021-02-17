6de0e495166890900be9a6d7741b42ced15fc6a4
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-17 16:42:15 -0500

    display stores with store name in bootstrap cards add link to /shop todo: add /shop routing

--- src/pages/StoresIndexPage/StoresIndexPage.jsx
+++ src/pages/StoresIndexPage/StoresIndexPage.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Card from 'react-bootstrap/Card';
+
 function StoresIndexPage(props) {
   console.log("Store Index Page: ", props)
   return (
     <div>
       <h1>Something for everyone!</h1>
-      <div>
-        {props.allStores.map(store => <div>{store.storeName}</div>)}
-      </div>
+        {props.allStores.map(store => {
+          if (store.storeName) {
+          console.log(store);
+          return (<Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>{store.storeName}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Store Category</Card.Subtitle>
+              <Card.Text>
+                Store Description Goes Here
+              </Card.Text>
+              <Card.Link href="/shop">Shop</Card.Link>
+            </Card.Body>
+          </Card>)
+          }
+        }
+      )}
     </div>
   )
 };

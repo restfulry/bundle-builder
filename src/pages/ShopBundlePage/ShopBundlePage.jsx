42b026452efc5f65bdf94bb0885c93415fb7e716
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-02-19 21:00:28 -0500

    show num times product has been added

--- src/pages/ShopBundlePage/ShopBundlePage.jsx
+++ src/pages/ShopBundlePage/ShopBundlePage.jsx
@@ -35,14 +35,13 @@ class ShopBundlePage extends Component {
     console.log('e targetKey', targetKey);
     console.log('e targetValue', targetValue);
     console.log('maxnUmProducts', this.state.bundle.maxNumProducts);
-
+    
     const formData = {...this.state.formData, [targetKey]: value};
-
+    
     this.setState({
       formData,
       invalidForm: !this.formRef.current.checkValidity()
     })
-    console.log("state", this.state.formData)
   };
 
   updateProductsSelected(key, product) {
@@ -53,17 +52,24 @@ class ShopBundlePage extends Component {
     }
   };
 
+  remainingChoices() {
+    let remainingChoices = this.state.bundle.maxNumProducts - this.state.formData.productsSelected.length;
+    return remainingChoices;
+  }
+
   maxProductsSelected() {
     let numProductsSelected = this.state.formData.productsSelected.length;
     if( numProductsSelected >= this.state.bundle.maxNumProducts) return true
   }
 
+  productQtySelected(productId) {
+    let productsSelected = this.state.formData.productsSelected;
+    let qtySelected = productsSelected.filter(selectedProductID => selectedProductID === productId);
 
-  // Select products to add to bundle
-  // Products get pushed to productsSelected
-  // disable when productsSelected.length === bundle.maxNumProducts
+    return qtySelected.length;
 
-
+  }
+  // if product in productsSelected, display count
 
   render() {
     const {bundle, products} = this.state;
@@ -72,6 +78,15 @@ class ShopBundlePage extends Component {
       <div>
         <h1>Build Your Bundle</h1>
         <h2>${bundle.price}</h2>
+
+        {this.remainingChoices() === 0 ? <h3>All Set!     <Button variant="primary" type="submit" className="btn" disabled={this.state.invalidForm}>Add to Cart</Button></h3>
+        :<div></div>
+        }
+        {this.remainingChoices() === 1 ?
+          <h3>Choose {this.remainingChoices()} More Item:</h3>
+        : 
+          <h3>Choose {this.remainingChoices()} More Items:</h3> 
+        }
 
         <Form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
 
@@ -86,7 +101,7 @@ class ShopBundlePage extends Component {
                 key={product._id} 
                 value={product._id} 
                 onClick={this.handleChange}>
-                Add
+                Add ({this.productQtySelected(`${product._id}`)})
               </Button>
 
             </div>
